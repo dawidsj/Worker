feef71d36aa0bee70e73931e79f3156bd77a52ee
unknown <dawidsj123@gmail.comgit config --global user.name Dawid>
2019-05-19 19:26:57 +0200

    navbar v1.1

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import * as $ from 'jquery';
 import {TokenService} from '../../services/token.service';
 import {Router} from '@angular/router';
+import {RegisterComponent} from '../register/register.component';
+import {LoginComponent} from '../login/login.component';
 
 
 @Component({
@@ -10,9 +12,13 @@ import {Router} from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  @ViewChild('loginComponent', { read: ViewContainerRef }) loginComponentRef;
+  @ViewChild('registerComponent', { read: ViewContainerRef }) registerComponentRef;
+  public showLogin: boolean = false;
+  public showRegister: boolean = false;
   constructor(public tokenService: TokenService,
-              public router: Router) { }
+              public router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver) { }
 
   public ngOnInit() {
     $(window).on('scroll', () => {
@@ -22,6 +28,37 @@ export class NavbarComponent implements OnInit {
         $('nav').removeClass('black');
       }
     });
+    $(function() {
+      if ($('body').hasClass('modal-open')) {
+        alert('x');
+      }
+    });
+    window.setInterval(function() {
+      $(function() {
+        if ($('#myLoginModal').hasClass('show')) {
+          alert('x');
+        }
+      });
+    }, 5000);
+    /*$('#myLoginModal').hasClass('show') {
+      /!*$(".modal-body").html("");*!/
+      alert('x');
+    };*/
+/*    $(!'#modalContent').on('click', function(){
+      alert('x');
+    });*/
+  }
+  loadLogin() {
+    this.showLogin = true;
+    const factory = this.componentFactoryResolver.resolveComponentFactory(LoginComponent);
+    const ref = this.loginComponentRef.createComponent(factory);
+    ref.changeDetectorRef.detectChanges();
+  }
+  loadRegister() {
+    this.showRegister = true;
+    const factory = this.componentFactoryResolver.resolveComponentFactory(RegisterComponent);
+    const ref = this.registerComponentRef.createComponent(factory);
+    ref.changeDetectorRef.detectChanges();
   }
   public logout() {
     this.tokenService.removeToken();
